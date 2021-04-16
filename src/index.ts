33a1ff2cf56ed19284f44513c6acf99058fef4a9
Arslan Ahmad <47880584+de-ar@users.noreply.github.com>
2021-04-17 03:21:04 +0500

    achieved max test coverage

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ const getIfHasJSON = (str: string): string | undefined => {
   start = str.indexOf('{', start + 1)
   while (start !== -1) {
     end = str.lastIndexOf('}')
-    if (end <= start) return undefined
     while (end > start) {
       try {
         return JSON.stringify(JSON.parse(str.substring(start, end + 1)))
--- test/index.test.js
+++ test/index.test.js
@@ -3,36 +3,56 @@ const getIfHasJSON = require('../lib/index.js')
 let param = ''
 let obj = {}
 
+// empty string
 test(`returns undefined with parameter: '${param}'`, () =>
   expect(getIfHasJSON(param)).toBeUndefined())
+
+// crap
 param = '{{{'
 test(`returns undefined with parameter: '${param}'`, () =>
   expect(getIfHasJSON(param)).toBeUndefined())
+
+// crap
 param = '!@#$%^&*()_+'
 test(`returns undefined with parameter: '${param}'`, () =>
   expect(getIfHasJSON(param)).toBeUndefined())
+
+// crap
 param = '}}}'
 test(`returns undefined with parameter: '${param}'`, () =>
   expect(getIfHasJSON(param)).toBeUndefined())
+
+// invalid JSON Object
 param = '{[]}'
 test(`returns undefined with parameter: '${param}'`, () =>
   expect(getIfHasJSON(param)).toBeUndefined())
+
+// invalid JSON Object
 param = '{"foo":bar}'
 test(`returns undefined with parameter: '${param}'`, () =>
   expect(getIfHasJSON(param)).toBeUndefined())
+
+// invalid JSON Object
 param = '{foo:"bar"}'
 test(`returns undefined with parameter: '${param}'`, () =>
   expect(getIfHasJSON(param)).toBeUndefined())
+
+// invalid JSON Object
 param = '{foo:"bar}'
 test(`returns undefined with parameter: '${param}'`, () =>
   expect(getIfHasJSON(param)).toBeUndefined())
 
+// empty JSON Object
 test(`returns parsable JSON string with parameter: '${JSON.stringify(
   obj,
 )}'`, () => expect(getIfHasJSON(JSON.stringify(obj))).toBe(JSON.stringify(obj)))
+
+// empty JSON Object with crap on both sides
 test(`returns parsable JSON string with parameter: '${
   'foo' + JSON.stringify(obj) + 'bar'
 }'`, () => expect(getIfHasJSON(JSON.stringify(obj))).toBe(JSON.stringify(obj)))
+
+// valid JSON Object with crap on both sides
 obj = { john: 'doe', foo: true }
 test(`returns parsable JSON string with parameter: '${
   'salkdj fslkf slkdfsd ' + JSON.stringify(obj) + '^&%$&^(*&^*&^'
