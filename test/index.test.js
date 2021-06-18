ebe727b41da665088842b3c7e75f87112103023b
Arslan Ahmad <47880584+de-ar@users.noreply.github.com>
2021-06-18 05:36:58 +0500

    update tests

--- test/index.test.js
+++ test/index.test.js
@@ -5,57 +5,58 @@ describe('#json', () => {
   let obj = {}
 
   // empty string
-  test(`returns undefined with parameter: '${param}'`, () =>
+  test(`returns undefined with invalid parameter: '${param}'`, () =>
     expect(json(param)).toBeUndefined())
 
   // crap
   param = '{{{'
-  test(`returns undefined with parameter: '${param}'`, () =>
+  test(`returns undefined with invalid parameter: '${param}'`, () =>
     expect(json(param)).toBeUndefined())
 
   // crap
   param = '!@#$%^&*()_+'
-  test(`returns undefined with parameter: '${param}'`, () =>
+  test(`returns undefined with invalid parameter: '${param}'`, () =>
     expect(json(param)).toBeUndefined())
 
   // crap
   param = '}}}'
-  test(`returns undefined with parameter: '${param}'`, () =>
+  test(`returns undefined with invalid parameter: '${param}'`, () =>
     expect(json(param)).toBeUndefined())
 
   // invalid JSON Object
   param = '{[]}'
-  test(`returns undefined with parameter: '${param}'`, () =>
+  test(`returns undefined with invalid parameter: '${param}'`, () =>
     expect(json(param)).toBeUndefined())
 
   // invalid JSON Object
   param = '{"foo":bar}'
-  test(`returns undefined with parameter: '${param}'`, () =>
+  test(`returns undefined with invalid parameter: '${param}'`, () =>
     expect(json(param)).toBeUndefined())
 
   // invalid JSON Object
   param = '{foo:"bar"}'
-  test(`returns undefined with parameter: '${param}'`, () =>
+  test(`returns undefined with invalid parameter: '${param}'`, () =>
     expect(json(param)).toBeUndefined())
 
   // invalid JSON Object
   param = '{foo:"bar}'
-  test(`returns undefined with parameter: '${param}'`, () =>
+  test(`returns undefined with invalid parameter: '${param}'`, () =>
     expect(json(param)).toBeUndefined())
 
   // empty JSON Object
-  test(`parsed JSON object with parameter: '${JSON.stringify(obj)}'`, () =>
-    expect(json(JSON.stringify(obj))).toStrictEqual(obj))
+  test(`returns parsed JSON object with valid parameter: '${JSON.stringify(
+    obj,
+  )}'`, () => expect(json(JSON.stringify(obj))).toStrictEqual(obj))
 
   // empty JSON Object with crap on both sides
-  test(`parsed JSON object with parameter: '${
+  test(`returns parsed JSON object with valid parameter: '${
     'foo' + JSON.stringify(obj) + 'bar'
   }'`, () =>
     expect(json('foo' + JSON.stringify(obj) + 'bar')).toStrictEqual(obj))
 
   // valid JSON Object with crap on both sides
   obj = { john: 'doe', foo: true }
-  test(`parsed JSON object with parameter: '${
+  test(`returns parsed JSON object with valid parameter: '${
     'salkdj fslkf slkdfsd ' + JSON.stringify(obj) + '^&%$&^(*&^*&^'
   }'`, () =>
     expect(
